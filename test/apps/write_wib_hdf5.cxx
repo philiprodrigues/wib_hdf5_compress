cb6fcb3b5b36e762690f0963ec088e497f5cc6d3
Philip Rodrigues <rodriges@fnal.gov>
2022-04-26 12:36:07 +0200

    Add partial outline of write_wib_hdf5.cxx

--- test/apps/write_wib_hdf5.cxx
+++ test/apps/write_wib_hdf5.cxx
@@ -1,6 +1,18 @@
+#include "CLI/CLI.hpp"
 
-int main()
+#include <string>
+
+int main(int argc, char** argv)
 {
+  CLI::App app{ "Write raw WIB data as HDF5 file" };
+
+  std::string in_filename;
+  app.add_option("-i,--input", in_filename, "Input raw WIB file");
+
+  std::string out_filename;
+  app.add_option("-o,--output", out_filename, "Output HDF5 file");
+
+  CLI11_PARSE(app, argc, argv);
   
   return 0;
 }
