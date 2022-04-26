1ce5d944c0c120bf80e3edf5b1b84cbe3cac5ba2
Philip Rodrigues <rodriges@fnal.gov>
2022-04-26 12:44:50 +0200

    Add outline of HDF5 code, copied from HighFive tests

--- test/apps/write_wib_hdf5.cxx
+++ test/apps/write_wib_hdf5.cxx
@@ -1,6 +1,16 @@
 #include "CLI/CLI.hpp"
 
 #include <string>
+#include <fstream>
+
+#include "detdataformats/wib/WIBFrame.hpp"
+
+#include "highfive/H5DataSet.hpp"
+#include "highfive/H5DataSpace.hpp"
+#include "highfive/H5File.hpp"
+#include "highfive/H5Group.hpp"
+#include "highfive/H5Reference.hpp"
+#include "highfive/H5Utility.hpp"
 
 int main(int argc, char** argv)
 {
@@ -13,6 +23,34 @@ int main(int argc, char** argv)
   app.add_option("-o,--output", out_filename, "Output HDF5 file");
 
   CLI11_PARSE(app, argc, argv);
-  
+
+  HighFive::File file(out_filename, HighFive::File::ReadWrite | HighFive::File::Create | HighFive::File::Truncate);
+
+  const size_t n_channels = 256;
+  const size_t n_frames = 3;
+  // Create the data space for the dataset.
+  std::vector<size_t> dims{ n_channels, n_frames };
+
+  HighFive::DataSpace dataspace(dims);
+
+  // Use chunking
+  HighFive::DataSetCreateProps props;
+  // props.add(HighFive::Chunking(std::vector<hsize_t>{ x_chunk, y_chunk }));
+
+  // Enable shuffle
+  props.add(HighFive::Shuffle());
+
+  // Enable deflate
+  props.add(HighFive::Deflate(3));
+
+  // Create a dataset with arbitrary type
+  HighFive::DataSet dataset = file.createDataSet<uint16_t>("wib_data", dataspace, props);
+
+  uint16_t array[n_channels][n_frames]={0};
+
+  dataset.write(array);
+
+  file.flush();
+
   return 0;
 }
